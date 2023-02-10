5c5092fdd35f50d41915d0c03f62b8f274db7b20
itshsinyu <itshsinyu@gmail.com>
2023-02-10 16:30:36 +0800

    0.1.3 (2023.02.10)

--- js/index-scroll.js
+++ js/index-scroll.js
@@ -1,9 +1,11 @@
 let memediv = document.getElementsByClassName("index-allmeme")[0];
 let leftImg01 = document.getElementsByClassName("left-img")[0];
 let leftImg02 = document.getElementsByClassName("left-img")[1];
+// let leftImgs = document.getElementsByClassName("left-img");
 let rightImg01 = document.getElementsByClassName("right-img")[0];
 let rightImg02 = document.getElementsByClassName("right-img")[1];
-console.log(memediv);
+let evergreen = document.getElementsByClassName("evergreen")[0];
+// console.log(evergreen);
 // scroll監聽事件
 function scrollDetect(){
     let lastScroll = 0;
@@ -14,24 +16,31 @@ function scrollDetect(){
         if (currentScroll > 0 && lastScroll <= currentScroll){
           lastScroll = currentScroll;
           console.log('bottom');
-          leftImg01.classList.add("transleft");
-          leftImg01.classList.remove("transright");
+          leftImg01.classList.add("trans-left");
+          leftImg02.classList.add("trans-left");
+          rightImg01.classList.add("trans-right");
+          rightImg02.classList.add("trans-right");
+          leftImg01.classList.remove("trans-right");
+          leftImg02.classList.remove("trans-right");
+          rightImg01.classList.remove("trans-left");
+          rightImg02.classList.remove("trans-left");
+          evergreen.classList.add("evergreen-left");
+          evergreen.classList.remove("trans-right");
         }else{
           lastScroll = currentScroll;
           console.log('top');
-          leftImg01.classList.add("transright");
-          leftImg01.classList.remove("transleft");
+          leftImg01.classList.add("trans-right");
+          leftImg02.classList.add("trans-right");
+          rightImg01.classList.add("trans-left");
+          rightImg02.classList.add("trans-left");
+          leftImg01.classList.remove("trans-left");
+          leftImg02.classList.remove("trans-left");
+          rightImg01.classList.remove("trans-right");
+          rightImg02.classList.remove("trans-right");
+          evergreen.classList.add("trans-right");
+          evergreen.classList.remove("evergreen-left");
         }
     };
-  }
-
-memediv.addEventListener("scroll", function(){
-    // if(memediv.getBoundingClientRect().top <= 500){
-        console.log('123');
-
-    // }
-});
+}
 
 scrollDetect();
-  
-  
